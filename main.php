ec0c805ba968bda399e39ec4afe95cae2bf6267a
MengShaoying <mengshaoying@aliyun.com>
2019-02-23 14:01:52 +0800

    change color

--- main.php
+++ main.php
@@ -10,10 +10,13 @@
  *
  * @param float $x
  * @param float $y
+ * @param float $z
  * @param float $a
  * @param float $b
  * @param float $c
  * @param float $d
+ * @param float $e
+ * @param float $f
  * @return array
  */
 function getNextPoint($x, $y, $a, $b, $c, $d)
@@ -28,12 +31,16 @@ function getNextPoint($x, $y, $a, $b, $c, $d)
  */
 class Image
 {
+    /** @var int image size */
+    const SIZE = 400;
     /** @var string file location */
     private $path;
     /** @var resource a GD resource */
     private $gd;
-    /** @var int color */
-    private $color;
+    /** @var array the image matrix */
+    private $matrix = [];
+    /** @var int max value in matrix */
+    private $max = 0;
 
     /**
      * Create a Image instance
@@ -42,10 +49,15 @@ class Image
      */
     public function __construct($path)
     {
-        $this->gd = imagecreate(4000, 4000);
-        imagecolorallocate($this->gd, 0, 0, 0);
-        $this->color = imagecolorallocate($this->gd, 255, 255, 255);
+        $this->gd = imagecreatetruecolor(self::SIZE, self::SIZE);
+        imageantialias($this->gd, true);
         $this->path = $path;
+        for ($i = 0; $i < self::SIZE; $i++) {
+            $this->matrix[] = [];
+            for ($j = 0; $j < self::SIZE; $j++) {
+                $this->matrix[$i][$j] = 1;
+            }
+        }
     }
 
     /**
@@ -53,10 +65,18 @@ class Image
      *
      * @param float $x
      * @param float $y
+     * @param float $z
      */
     public function draw($x, $y)
     {
-        imagesetpixel($this->gd, intval(1000 * $x + 2000), intval(1000 * $y + 2000), $this->color);
+        $xIndex = intval(($x + 2) / 4 * self::SIZE);
+        $xIndex = $xIndex < 0 ? 0 : ($xIndex >= self::SIZE ? self::SIZE - 1 : $xIndex);
+        $yIndex = intval(($y + 2) / 4 * self::SIZE);
+        $yIndex = $yIndex < 0 ? 0 : ($yIndex >= self::SIZE ? self::SIZE - 1 : $yIndex);
+        $this->matrix[$yIndex][$xIndex]++;
+        if (log($this->matrix[$yIndex][$xIndex], 2) > $this->max) {
+            $this->max = log($this->matrix[$yIndex][$xIndex], 2);
+        }
     }
 
     /**
@@ -64,6 +84,14 @@ class Image
      */
     public function save()
     {
+        for ($i = 0; $i < self::SIZE; $i++) {
+            for ($j = 0; $j < self::SIZE; $j++) {
+                $color = intval(log($this->matrix[$i][$j], 2) * 255 / $this->max);
+                $color = imagecolorallocate($this->gd, $color, $color, $color);
+                imagesetpixel($this->gd, $j, $i, $color);
+                imagecolordeallocate($this->gd, $color);
+            }
+        }
         imagepng($this->gd, $this->path);
     }
 }
@@ -75,9 +103,14 @@ $b = mt_rand() / mt_getrandmax() * 6 - 3;
 $c = mt_rand() / mt_getrandmax() * 6 - 3;
 $d = mt_rand() / mt_getrandmax() * 6 - 3;
 
-$image = new Image('Peter_De_Jong.png');
+$fileNameId = 1;
+while (file_exists("Peter_De_Jong_$fileNameId.png")) {
+    $fileNameId++;
+}
+
+$image = new Image("Peter_De_Jong_$fileNameId.png");
 $data = [$x, $y];
-for ($i = 1; $i < 2000000; $i++) {
+for ($i = 1; $i < 100000; $i++) {
     list($x, $y) = $data;
     $image->draw($x, $y);
     $data = getNextPoint($x, $y, $a, $b, $c, $d);
